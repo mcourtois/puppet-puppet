f47f2da6373d1c83215bd0a919509956730a9a36
Marc-André Courtois <macourtois@iweb.com>
2013-05-06 13:36:03 -0400

    Fixed certificate request to the CA for the master

--- manifests/master/certificate.pp
+++ manifests/master/certificate.pp
@@ -1,18 +1,29 @@
 # === Class: puppet::master::certificate
 class puppet::master::certificate (
-  $waitforcert = '120',
+  $waitforcert = '10',
 ) {
   include puppet
   $wait = "--waitforcert ${waitforcert}"
-  $command = "/usr/bin/puppet agent --ca_server ${puppet::ca_server}"
+  $dns_alt_names = $puppet::agent_options['dns_alt_names']
+  $command = "/usr/bin/puppet agent --ca_server ${puppet::ca_server} --server ${puppet::ca_server} --dns_alt_names ${dns_alt_names} --onetime --no-daemonize --noop"
 
   $puppet_ssl = $::settings::ssldir
 
+  Package['puppet']->
+  Notify['about-to-send-csr']->
+  Exec['puppet-cert-request']->
+  Exec['create-ca-directory']->
+  Exec['link-ca-crl']
+
+  notify { 'about-to-send-csr':
+    message => "Requesting certificate signature on the CA. You have ${waitforcert} seconds to go sign the request with --allow-dns-alt-names.",
+  }
+
   exec { 'puppet-cert-request':
-    command => "${command} ${wait} ${puppet::certname}",
-    creates => "${puppet_ssl}/certs/${puppet::certname}.pem",
-    timeout => $waitforcert + 60,
-    require => Package['puppet'],
+    command   => "${command} ${wait} ${puppet::certname}",
+    creates   => "${puppet_ssl}/certs/${puppet::certname}.pem",
+    timeout   => $waitforcert + 120,
+    logoutput => true,
   }
 
   exec { 'create-ca-directory':
@@ -24,7 +35,6 @@ class puppet::master::certificate (
     command => '/bin/ln -s ../crl.pem ./ca/ca_crl.pem',
     creates => "${puppet_ssl}/ca/ca_crl.pem",
     cwd     => $puppet_ssl,
-    require => Exec['create-ca-directory', 'puppet-cert-request'],
   }
 }
 
